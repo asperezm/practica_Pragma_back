9f855cc3a4f7c2345ae15eb8f9d018c5841fbb92
asperezm <47333056+asperezm@users.noreply.github.com>
2021-07-06 12:39:35 -0500

    image config

--- image-service/src/main/java/com/example/imageservice/entity/Photo.java
+++ image-service/src/main/java/com/example/imageservice/entity/Photo.java
@@ -5,9 +5,12 @@ import javax.persistence.Id;
 import org.bson.types.Binary;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
+@Document(collation = "photos")
 public class Photo {
     
     @Id
