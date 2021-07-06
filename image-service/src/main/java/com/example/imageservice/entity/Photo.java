8c0cc97354b9f20ea459bff16d0bdefb82b47c4b
asperezm <47333056+asperezm@users.noreply.github.com>
2021-07-06 12:48:36 -0500

    delete dependency and change properties

--- image-service/src/main/java/com/example/imageservice/entity/Photo.java
+++ image-service/src/main/java/com/example/imageservice/entity/Photo.java
@@ -1,14 +1,18 @@
 package com.example.imageservice.entity;
 
-import javax.persistence.Id;
 
 import org.bson.types.Binary;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Document(collation = "photos")
 public class Photo {
