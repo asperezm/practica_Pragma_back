5b634d9be3c9e665c318de011f3d3e6049d21a1f
asperezm <asperem@eafit.edu.co>
2021-11-12 23:03:50 -0500

    registro prueba

--- registry-service/src/main/java/com/example/registryservice/RegistryServiceApplication.java
+++ registry-service/src/main/java/com/example/registryservice/RegistryServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class RegistryServiceApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(RegistryServiceApplication.class, args);
 	}
